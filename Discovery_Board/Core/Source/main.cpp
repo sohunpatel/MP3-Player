d54249ab6f1738899badb6a7435a54fcc246ba6b
Sohun <sohunpatel@gmail.com>
2021-04-03 23:39:56 -0700

    Add CMSIS RTOS2

--- Discovery_Board/Core/Source/main.cpp
+++ Discovery_Board/Core/Source/main.cpp
@@ -1,10 +1,28 @@
+/**-----------------------------------------------------------------------------
+ * @file    :  main.cpp
+ * @author  :  Sohun Patel
+ * @date    :  03 April 2021
+ * @brief   :  main function
+ * -----------------------------------------------------------------------------
+ **/
+
+/*----- Includes -------------------------------------------------------------*/
+#include "cmsis_os2.h"
 #include "main.h"
 #include "stdint.h"
 
-int main() {
-    uint32_t temp = 32;
-    for (int i = 0; i < 100; i++) {
-        temp++;
-    }
-    while (1);
+void MP3_Decoding_Thread(void *argument);
+void MP3_Queueing_Thread(void *argument);
+
+osThreadId_t MP3_Decoding;
+osThreadId_t MP3_Queueing;
+
+int main() {    
+    MP3_Decoding = osThreadNew(MP3_Decoding_Thread, NULL, NULL);
+    MP3_Queueing = osThreadNew(MP3_Queueing_Thread, NULL, NULL);
+
+    /*----- Start scheduler --------------------------------------------------*/    
+    osKernelStart();
+
+    while (true);
 }
